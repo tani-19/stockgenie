2e87da6bfff6bd218fead35549f716aa1c7725f2
tani-19 <tanishkaratnani04@gmail.com>
2025-03-03 12:43:31 +0530

    .

--- Stockpulse_v1-main/stockpulseapp/env/bin/activate.fish
+++ Stockpulse_v1-main/stockpulseapp/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tanishkaratnani/Desktop/Stockpulse_v1-main/stockpulseapp/env"
+set -gx VIRTUAL_ENV "/Users/tanishkaratnani/Desktop/stockgenie/Stockpulse_v1-main/stockpulseapp/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
